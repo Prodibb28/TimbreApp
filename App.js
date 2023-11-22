cd3ae72bc700f1abd5f6079f10010a4427ad5718
Jaider Hoyos <70029242+Prodibb28@users.noreply.github.com>
2023-11-21 23:35:57 -0500

    changedOna

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect} from "react"
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { firebase } from './src/config/config';
 
-export default function App() {
+import Login from './src/screen/Login';
+import SingUp from './src/screen/SingUp';
+import Home from './src/screen/Home';
+
+const Stack = createStackNavigator();
+
+function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+   }
+
+   useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if(initializing) return null;
+
+  if(!user){
+  return(
+    <Stack.Navigator
+    screenOptions={{headerShown: false}}
+    >
+
+      <Stack.Screen 
+       name="Login" 
+       component={Login} 
+      /> 
+      <Stack.Screen 
+       name="SingUp" 
+       component={SingUp} 
+      /></Stack.Navigator>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator
+    screenOptions={{headerShown: false}}>
+      <Stack.Screen 
+       name="Home" 
+       component={Home} 
+      /> 
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default() => {
+  return (
+    <NavigationContainer>
+      <App/>
+    </NavigationContainer>
+  )
+}
