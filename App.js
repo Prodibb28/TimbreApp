097770b801fbe506311b2228d6b0b40d6670eb54
Jaider Hoyos <70029242+Prodibb28@users.noreply.github.com>
2023-11-23 11:53:55 -0500

    Bottomtab Nav added

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect} from "react"
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { firebase } from './src/config/config';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Login from './src/screen/Login';
 import SingUp from './src/screen/SingUp';
 import Home from './src/screen/Home';
+import Registro from './src/screen/Registro';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
   const [initializing, setInitializing] = useState(true);
@@ -27,6 +31,7 @@ function App() {
 
   if(!user){
   return(
+    
     <Stack.Navigator
     screenOptions={{headerShown: false}}
     >
@@ -42,7 +47,41 @@ function App() {
     );
   }
   return (
-    <Stack.Navigator>
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      activeTintColor: '#1976D2', // Color del ícono y el texto cuando está seleccionado
+      inactiveTintColor: 'gray',// Color del ícono y el texto cuando no está seleccionado
+      tabBarStyle: { position: 'absolute',  height: 55},
+      tabBarLabelStyle : { fontSize: 12, marginBottom:5 },  // Tamaño de la letra
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Home') {
+          iconName = focused ? 'people' : 'people-outline';
+        } else if (route.name === 'Registro') {
+          iconName = focused ? 'podium' : 'podium-outline';
+        }
+
+        // Puedes cambiar el tamaño de los iconos ajustando el valor de 'size'
+        return <Icon style={{marginTop:5,}} name={iconName} size={size} color={color} />;
+      },
+    })}
+
+    >
+      <Tab.Screen name="Home" component={Home} 
+          options={{
+            headerTitleAlign:'center',
+            
+          }}
+        />
+        <Tab.Screen name="Registro" component={Registro} 
+          options={{
+            headerTitleAlign:'center'
+          }}
+        />
+
+      </Tab.Navigator>
+    /*<Stack.Navigator>
       <Stack.Screen 
        name="Home" 
        component={Home} 
@@ -50,7 +89,7 @@ function App() {
         headerTitleAlign:'center',
       }}
       /> 
-    </Stack.Navigator>
+    </Stack.Navigator>*/
   );
 }
 
---
+++ src/screen/Registro.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+const Registro = () => {
+  return (
+    <View style={styles.contentContainer}>
+        <Text style={styles.title}>Welcome to Registro</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+    alignItems:'center',
+    backgroundColor:'white',
+    borderBottomColor:'black',
+    borderColor:"#9eb4c1",
+    borderWidth: 0.8,
+  },
+  cardsContainer:{
+
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+});
+
+export default Registro;
